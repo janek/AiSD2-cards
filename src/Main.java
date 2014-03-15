2e8a3816774460cec16285f916286c300435fa12
Janek Szynal <jan.szynal@gmail.com>
2014-03-15 15:37:33 +0100

    Implemented PlayingCard

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
-
 public class Main {
-
+	
+	public static void main (String[] args){
+		CardGame game = new CardGame();
+		PlayingCard card = game.createCard();
+		System.out.println(card.description);
+		//menu
+		//1. Utworzenie listy
+		//2. Wyæwietlanie listy
+		//3. Wyæwietlanie liczby element—w listy
+		//4. Wyæwietlanie kart o podanej wartoæci
+		//5. Wyæwietlanie kart o podanym kolorze
+	}
+		
 }
--- src/PlayingCard.java
+++ src/PlayingCard.java
@@ -1,4 +1,18 @@
 
 public class PlayingCard {
-
+	
+	int rank;
+	int suit;
+	
+	String[] rankStrings = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+	String[] suitStrings = {"Karo", "Kier", "Pik", "Trefl"};
+	
+	String description;
+	
+	public PlayingCard(int rank, int suit) {
+		this.rank = rank;
+		this.suit = suit;
+		description = rankStrings[rank]+" "+suitStrings[suit];
+	}
+	
 }
