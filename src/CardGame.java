733d9a0dd6dcc4a7282d94c81eb5d8ed6366997e
Janek Szynal <jan.szynal@gmail.com>
2014-03-15 16:30:08 +0100

    Outlined method insertCard, started work on menu

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+
+
 
 public class CardGame {
 
+	ArrayList<PlayingCard> cards = new ArrayList<PlayingCard>();
+	
+//	Utworzonˆ kart« naleıy wstawi do listy na odpowiednie miejsce, tak aby karty 
+//	by¸y uporzˆdkowane wed¸ug wartoæci, a nast«pnie wed¸ug kolor—w.
+//
+//	Wylosowanie wartoæci zero dla pola Wartosc oznacza koniec tworzenia listy.
+
+	
 	PlayingCard createCard(){
-    	int rank = 0;// (int) (Math.random()*14);  
+    	int rank = 0;//(int) (Math.random()*14);  
 		int suit = (int) (Math.random()*4);
 		if (rank != 0){
 			return new PlayingCard(rank-1, suit);
 		} else{
 			return null;
 		}
-    	
     }
+	
+	void insertCard() {
+		if (createCard()!=null) {
+			System.out.println("a card is to be added");
+			insertCard();
+		} else
+		{
+			System.out.print("END");
+		}
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
 	
 	public static void main (String[] args){
 		CardGame game = new CardGame();
-		PlayingCard card = game.createCard();
-		System.out.println(card.description);
+//		PlayingCard card = game.createCard();
+//		System.out.println(card.description);
+		game.insertCard();
+		
 		//menu
 		//1. Utworzenie listy
 		//2. Wyæwietlanie listy
